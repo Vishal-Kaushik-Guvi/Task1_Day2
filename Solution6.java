70cb0d204d7ebdb219e5e74621ff561012ae9578
Vishal-Kaushik-Guvi <160844526+Vishal-Kaushik-Guvi@users.noreply.github.com>
2024-02-23 16:35:16 +0530

    Add files via upload
    all solved

--- Solution6.java
+++ Solution6.java
@@ -10,14 +10,12 @@ package Task1_Day2;
 
 public class Solution6 {
     public static void main(String[] args) {
-       int n=5;
-        for(int i=1; i<=5; i++){
-            System.out.print(5);
-            for(int j=1; j<5; j++){
-                System.out.print(n);  
+       int n=55555;
+       int m=1111;
+            for(int j=1; j<=5; j++){
+             System.out.println(n);
+             n=n-m;
+             m=m/10;
             }
-            --n;
-            System.out.println();
-        }
     }
 }
